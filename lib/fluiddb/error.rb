25b5ed7247f67f21ce63e56d8e9ad734eeb5ee30
Guillermo Álvarez <guillermo@cientifico.net>
2009-09-14 01:47:46 +0200

    Remove deprecated FDB::Error.check_errors

--- lib/fluiddb/error.rb
+++ lib/fluiddb/error.rb
@@ -6,10 +6,6 @@ module FluidDB
       info = "#{info[:errorCode]}: #{info[:errorClass]} at #{info[:path]}}" if info.is_a? Hash
       super(info)
     end
-    
-    def self.check_errors(ret)
-      raise FluidDB::Error.new(ret) if ret && ret[:errorCode]
-    end
   end
   
 end
