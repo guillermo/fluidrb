fd520fcfa4129b759350f594c12ac030a837473c
Guillermo Álvarez <guillermo@cientifico.net>
2009-09-14 00:58:44 +0200

    Refactor tag

--- lib/fluiddb/tag.rb
+++ lib/fluiddb/tag.rb
@@ -1,32 +1,58 @@
 module FluidDB 
-  class Tag < Resource
+  class Tag
+    def self.create(namespace,options)
+      connection = options.delete(:connection) || FluidDB.connection
+      obj = connection.post("/tags/#{namespace}",:payload => options)
+      new({:connection => connection, :namespace => namespace}.merge(options).merge(obj))
+    end
     
-    def self.create!(tag,description,indexed = true)
-      path = tag.split('/')[0..-2].join('/')
-      name = tag.split('/').last
+    def initialize(options = {})
+      @options = options
+      @options[:connection] ||= FluidDB.connection
+      if @options[:path]
+        path = @options[:path].split('/')
+        @options[:namespace] = path[0..-2].join('/')
+        @options[:name] = path.last
+      end
+    end
+    
+    def self.get(path,options = {})
+      connection = options.delete(:connection) || FluidDB.connection
+      obj = connection.get("/tags/#{path}",:query => options)
+      new({:connection => connection, :path => path}.merge(options).merge(obj))
+    end
+    
+    def delete
+      @options[:connection].delete("/tags/#{path}") && true
+    end
       
-      new_tag = new(:path => '/tags/'+path, :description => description, :indexed => indexed)
-      new_tag.post!(:description => description, :name => name, :indexed => indexed)
+    def namespace
+      @options[:namespace] ||= URI.parse(@options[:URI]).path.split('/')[0..-2].join('/')
     end
     
-    def self.find(tag)
-      new(:path => '/tags/'+tag).fetch
+    def name
+      @options[:name] ||= URI.parse(@options[:URI]).split('/').last
     end
     
-    def fetch
-      get!(:returnDescription => true)
-      self
+    def path
+      "#{namespace}/#{name}"
+    end
+    
+    def description
+      @options[:description] or raise FluidDB::Error.new('You should get with option :returnDescription => true')
     end
       
-    def update!(description)
-      put!(:description=>description)
-      self
-    end
-      
-    def destroy!
-      ret = delete!
-      true
+    def description=(description)
+      @options[:connection].put("/tags/#{path}",:payload => {:description => description})
+      @options[:description] = description
     end
     
+    def id
+      @options[:id]
+    end
+    
+    def indexed?
+      @options[:indexed]
+    end 
   end
 end
--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -1,53 +1,102 @@
 require File.join(File.dirname(__FILE__),'spec_helper')
 
 describe FluidDB::Tag do
-  
-  it 'should be able to find' do
-    name = generate_uniq
-    FluidDB::Tag.create!("test/#{name}",'description')
-    
-    tag = FluidDB::Tag.find("test/#{name}")
-    tag.should be_kind_of(FluidDB::Tag)
-    tag.description.should == 'description'
+
+  before(:all) do
+    @name = generate_uniq
+    @description = "description of #{@name}"
+    @created_tag = FluidDB::Tag.create('test', :description => @description, :indexed => true, :name => @name)
+    @getted_tag  = FluidDB::Tag.get("test/#{@name}", :returnDescription => true)
   end
   
-  it 'should create a new tag' do
-    name = generate_uniq
-    tag = FluidDB::Tag.create!("test/#{name}",'description')
-    
-    tag.should be_kind_of(FluidDB::Tag)
-    tag.description.should == 'description'
-    tag.indexed.should == true
-  end
-  
-  it 'should edit a tag' do
-    name = generate_uniq
-    tag = FluidDB::Tag.create!("test/#{name}",'description')
-    
-    tag.update!('new description')
-    FluidDB::Tag.find("test/#{name}").description.should == 'new description'
-  end
-  
-  it 'should remove a tag' do
-    name = generate_uniq
-    tag = FluidDB::Tag.create!("test/#{name}",'description')
-    tag.destroy!.should == true
+  it 'should be able to get a tag' do
+    @getted_tag.should be_kind_of(FluidDB::Tag)
   end
 
-  describe 'should raise error' do
-    it 'on create if already exists' do
-      name = generate_uniq
-      tag = FluidDB::Tag.create!("test/#{name}",'description')
-      
-      lambda{ FluidDB::Tag.create!("test/#{name}",'description') }.should raise_error( FluidDB::Error )      
+
+  it 'should be create a tag in a namespaces' do
+    @created_tag.should be_kind_of(FluidDB::Tag)
+  end
+  
+  it 'should raise if tried to find a non existing tag' do
+    lambda { FluidDB::Tag.get('test/wadus/wadus')}.should raise_error(FluidDB::Error)
+  end
+  
+  describe 'on getted object' do
+    it 'should have a namespace' do
+      @getted_tag.namespace.should == 'test'
+    end
+
+    it 'should have a name' do
+      @getted_tag.name.should == @name
+    end
+
+    it 'should have a path' do
+      @getted_tag.path.should == "test/#{@name}"
+    end
+
+    it 'should have a description' do
+      @getted_tag.description.should == @description
     end
     
-    it 'on remove if no exists' do
-      name = generate_uniq
-      tag = FluidDB::Tag.create!("test/#{name}",'description')
-      tag.destroy!.should == true
-      lambda{ tag.destroy! }.should raise_error( FluidDB::Error)
+    it 'should have indexed option' do
+      @getted_tag.indexed?.should == true
     end
+    
+    it 'should have id' do
+      @getted_tag.id.should match(FluidDB::Object::ID_FORMAT)
+    end
+  
+  end
+  
+  describe 'on created object' do
+    it 'should have a namespace' do
+      @created_tag.namespace.should == 'test'
+    end
+
+    it 'should have a name' do
+      @created_tag.name.should == @name
+    end
+
+    it 'should have a path' do
+      @created_tag.path.should == "test/#{@name}"
+    end
+
+    it 'should have a description' do
+      @created_tag.description.should == @description
+    end
+
+    it 'should have indexed option' do
+      @created_tag.indexed?.should == true
+    end
+    
+    it 'should have id' do
+      @created_tag.id.should match(FluidDB::Object::ID_FORMAT)
+    end
+  end
+  
+  
+  it 'should raise an error if try to get a description whitout explicity getting description' do
+    lambda { 
+      tag = FluidDB::Tag.get(@name)
+      tag.description
+    }.should raise_error(FluidDB::Error)
+  end
+  
+  it 'should edit description' do
+    name = generate_uniq+"2"
+    tag = FluidDB::Tag.create('test', :description => 'my description', :indexed => true, :name => name)
+    tag.description = 'wadus wadus description'
+    tag.description.should == 'wadus wadus description'
+    
+    tag = FluidDB::Tag.get("test/#{name}", :returnDescription => true)
+    tag.description.should == 'wadus wadus description'
+  end
+  
+  it 'should be deleted' do
+    name = generate_uniq+"3"
+    tag = FluidDB::Tag.create('test', :description => 'my description', :indexed => true, :name => name)
+    tag.delete.should == true
   end
   
 end
