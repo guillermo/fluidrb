8e19f5928813c14b533941ff87d6d0484ac8e520
Guillermo Álvarez <guillermo@cientifico.net>
2009-09-19 17:03:38 +0200

    Refactor FDB::User

--- lib/fluiddb/user.rb
+++ lib/fluiddb/user.rb
@@ -1,12 +1,25 @@
 module FluidDB
   class User #:nodoc:
-    def self.find(name)
-      new(:path => '/users/'+name).fetch
+    def self.get(username,options ={})
+      connection = options.delete(:connection) || FluidDB.connection
+      new( {:username => username}.merge( connection.get("/users/#{username}") ) )
     end
     
-    def fetch
-      get!
-      self
+    def username
+      @options[:username]
     end
+    
+    def name
+      @options[:name]
+    end
+    
+    def id
+      @options[:id]
+    end
+    
+    def initialize(options) #:nodoc:
+      @options = options
+    end
+    
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,14 +1,26 @@
 require File.join(File.dirname(__FILE__),'spec_helper')
 
 describe FluidDB::User do
+  before(:all) do
+    @user = FluidDB::User.get('test')
+  end
   
   it 'should return an User if exists' do
-    u = FluidDB::User.find('test')
-    u.should be_kind_of(FluidDB::User)
-    u.name.should == 'test'
+    @user.should be_kind_of(FluidDB::User)
+  end
+  
+  it 'should have username' do
+    @user.username.should == 'test'
+  end
+  it "should have name" do
+    @user.name == 'test'
+  end
+  
+  it 'should have an id' do
+    @user.id.should match(FluidDB::Object::ID_FORMAT)
   end
   
   it 'should raise an error if an User doesn\'t exists' do
-    lambda { FluidDB::User.find('probablyanonexistinguser') }.should raise_error(FluidDB::Error)
+    lambda { FluidDB::User.get('probablyanonexistinguser') }.should raise_error(FluidDB::Error)
   end
 end
