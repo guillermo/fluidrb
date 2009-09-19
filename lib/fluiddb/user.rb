998cc065ffda4e1342ad463e92ffcc2850be86ac
Guillermo Álvarez <guillermo@cientifico.net>
2009-09-19 17:40:22 +0200

    Include user documentation

--- lib/fluiddb/user.rb
+++ lib/fluiddb/user.rb
@@ -1,18 +1,22 @@
 module FluidDB
-  class User #:nodoc:
+  class User
+    # Return an user by its username
     def self.get(username,options ={})
       connection = options.delete(:connection) || FluidDB.connection
       new( {:username => username}.merge( connection.get("/users/#{username}") ) )
     end
     
+    # Return the username
     def username
       @options[:username]
     end
     
+    # Return the name
     def name
       @options[:name]
     end
     
+    # Return the id
     def id
       @options[:id]
     end
